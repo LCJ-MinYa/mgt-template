0692379042dee0e2a59d77ed7524e474ba6be402
lichaojun <lichaojun@cdqcp.com>
2022-11-11 14:38:27 +0800

    feat: mockjs服务端模拟1秒接口返回

--- mock/table.js
+++ mock/table.js
@@ -1,30 +1,36 @@
 const Mock = require('mockjs');
 
-const data = Mock.mock({
-    'list|10': [
-        {
+const List = [];
+const count = 96;
+
+for (let i = 0; i < count; i++) {
+    List.push(
+        Mock.mock({
             id: '@id',
             title: '@sentence(10, 20)',
             'status|1': ['published', 'draft', 'deleted'],
-            author: 'name',
+            author: '@cname()',
             display_time: '@datetime',
             pageviews: '@integer(300, 5000)',
             audit_time: '@datetime',
-        },
-    ],
-});
+        })
+    );
+}
 
 module.exports = [
     {
         url: '/vue-admin-template/table/list',
         type: 'get',
         response: (config) => {
-            const { list } = data;
+            const { currentPage = 1, pageSize = 10 } = config.query;
+
+            const pageList = List.filter((item, index) => index < pageSize * currentPage && index >= pageSize * (currentPage - 1));
+
             return {
                 code: 20000,
                 data: {
-                    total: list.length,
-                    list: data.list,
+                    total: List.length,
+                    list: pageList,
                 },
             };
         },
