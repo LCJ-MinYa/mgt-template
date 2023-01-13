092a9df434ed728e783739e8b843db489b925a42
lichaojun <lichaojun@cdqcp.com>
2023-01-13 09:39:28 +0800

    feat: extend模板继承再次优化

--- src/views/example/extend/index.vue
+++ src/views/example/extend/index.vue
@@ -1,33 +1,19 @@
 <script>
-import ParentClass from './parentClass.vue';
+import BaseClassDemo from './baseClass.vue';
+import RenderMixin from './renderMixin';
 
 export default {
-    extends: ParentClass,
+    extends: BaseClassDemo,
+    mixins: [RenderMixin],
     data() {
         return {};
     },
-    render(h) {
-        this.$slots.default = <elCard class="mb-20">子类自定义slot</elCard>;
-        this.$slots.otherSlotA = <elCard class="mb-20">子类自定义otherSlotA</elCard>;
-        this.$slots.otherSlotB = <elCard>子类自定义otherSlotB</elCard>;
-        const baseClass = this.$options.extends;
-        /**
-         * 写法一 不推荐
-         * return baseClass.render.apply(this, [this.$slots.default, this.$slots.otherSlotA, this.$slots.otherSlotB]);
-         */
-
-        /**
-         * 写法二 如果引入的基类名称改变了，这里也需要跟着改变，不推荐
-         * return ParentClass.render.call(this, h);
-         */
-
-        /** 写法三 推荐 */
-        return baseClass.render.call(this, h);
-    },
-
     mounted() {},
-
-    methods: {},
+    methods: {
+        renderDefault() {
+            return <elCard class="mb-20">子类自定义slot</elCard>;
+        },
+    },
 };
 </script>
 
---
+++ src/views/example/extend/renderMixin.js
@@ -0,0 +1,43 @@
+export default {
+    render(h) {
+        this.$slots.default = this.renderDefault();
+        this.$slots.otherSlotA = this.renderOtherSlotA();
+        this.$slots.otherSlotB = this.renderOtherSlotB();
+        const baseClass = this.$options.extends;
+        /**
+         * 写法一 不推荐
+         * return baseClass.render.apply(this, [this.$slots.default, this.$slots.otherSlotA, this.$slots.otherSlotB]);
+         */
+
+        /**
+         * 写法二 如果引入的基类名称改变了，这里也需要跟着改变，不推荐
+         * return ParentClass.render.call(this, h);
+         */
+
+        /** 写法三 推荐 */
+        return baseClass.render.call(this, h);
+    },
+    methods: {
+        renderDefault() {
+            /**
+             * 默认应该 return null;
+             * 此处为了演示，返回一个mixin自定义的slot
+             */
+            return <elCard class="mb-20">子类Mixin自定义slot</elCard>;
+        },
+        renderOtherSlotA() {
+            /**
+             * 默认应该 return null;
+             * 此处为了演示，返回一个mixin自定义的slot
+             */
+            return <elCard class="mb-20">子类Mixin自定义otherSlotA</elCard>;
+        },
+        renderOtherSlotB() {
+            /**
+             * 默认应该 return null;
+             * 此处为了演示，返回一个mixin自定义的slot
+             */
+            return <elCard class="mb-20">子类Mixin自定义otherSlotB</elCard>;
+        },
+    },
+};
