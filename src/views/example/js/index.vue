c731b90729d52d76da77a9808c66f75a91aeb6f6
lichaojun <lichaojun@cdqcp.com>
2023-01-16 18:01:59 +0800

    feat: js相关

--- src/views/example/js/index.vue
+++ src/views/example/js/index.vue
@@ -1,57 +1,18 @@
 <template>
     <base-container>
         <base-main>
-            <el-container>
-                <el-aside>
-                    <el-menu :default-openeds="['1 ', '3']">
-                        <el-submenu index="1">
-                            <template slot="title"><i class="el-icon-message"></i>导航一</template>
-                            <el-menu-item-group>
-                                <template slot="title">分组一</template>
-                                <el-menu-item index="1-1">选项1</el-menu-item>
-                                <el-menu-item index="1-2">选项2</el-menu-item>
-                            </el-menu-item-group>
-                            <el-menu-item-group title="分组2">
-                                <el-menu-item index="1-3">选项3</el-menu-item>
-                            </el-menu-item-group>
-                            <el-submenu index="1-4">
-                                <template slot="title">选项4</template>
-                                <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-                            </el-submenu>
-                        </el-submenu>
-                        <el-submenu index="2">
-                            <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-                            <el-menu-item-group>
-                                <template slot="title">分组一</template>
-                                <el-menu-item index="2-1">选项1</el-menu-item>
-                                <el-menu-item index="2-2">选项2</el-menu-item>
-                            </el-menu-item-group>
-                            <el-menu-item-group title="分组2">
-                                <el-menu-item index="2-3">选项3</el-menu-item>
-                            </el-menu-item-group>
-                            <el-submenu index="2-4">
-                                <template slot="title">选项4</template>
-                                <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-                            </el-submenu>
-                        </el-submenu>
-                        <el-submenu index="3">
-                            <template slot="title"><i class="el-icon-setting"></i>导航三</template>
-                            <el-menu-item-group>
-                                <template slot="title">分组一</template>
-                                <el-menu-item index="3-1">选项1</el-menu-item>
-                                <el-menu-item index="3-2">选项2</el-menu-item>
-                            </el-menu-item-group>
-                            <el-menu-item-group title="分组2">
-                                <el-menu-item index="3-3">选项3</el-menu-item>
-                            </el-menu-item-group>
-                            <el-submenu index="3-4">
-                                <template slot="title">选项4</template>
-                                <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-                            </el-submenu>
-                        </el-submenu>
-                    </el-menu></el-aside
-                >
-                <el-main class="p-0">11</el-main>
+            <el-container class="h-full">
+                <el-aside class="mr-20 bg-white">
+                    <el-menu unique-opened :default-active="menuIndex" @select="changeMenu">
+                        <el-menu-item v-for="item in menuList" :key="item.index" :index="item.index">
+                            <span slot="title">{{ item.title }}</span>
+                        </el-menu-item>
+                    </el-menu>
+                </el-aside>
+                <el-main class="bg-white">
+                    <div v-if="menuIndex === 'object'">查看控制台输出{{ handleNewObj() }}{{ handleNewObjWithFuc() }}</div>
+                    <div v-if="menuIndex === 'event-bus'">2</div>
+                </el-main>
             </el-container>
         </base-main>
     </base-container>
@@ -60,10 +21,54 @@
 <script>
 export default {
     data() {
-        return {};
+        return {
+            menuIndex: 'object',
+            menuList: [
+                {
+                    index: 'object',
+                    title: '面向对象',
+                },
+                {
+                    index: 'event-bus',
+                    title: '实现event-bus',
+                },
+            ],
+        };
     },
     mounted() {},
-    methods: {},
+    methods: {
+        changeMenu(index) {
+            if (index !== this.menuIndex) {
+                this.menuIndex = index;
+            }
+        },
+        /** object */
+        handleNewObj() {
+            function person(name) {
+                this.name = name;
+                this.logName = function () {
+                    console.log(this.name);
+                };
+            }
+
+            const xiaoming = new person('小明');
+            const xiaohong = new person('小红');
+            console.log(xiaoming.logName === xiaohong.logName);
+        },
+        handleNewObjWithFuc() {
+            function person(name) {
+                this.name = name;
+            }
+
+            person.prototype.logName = function () {
+                console.log(this.name);
+            };
+
+            const xiaoming = new person('小明');
+            const xiaohong = new person('小红');
+            console.log(xiaoming.logName === xiaohong.logName);
+        },
+    },
 };
 </script>
 
